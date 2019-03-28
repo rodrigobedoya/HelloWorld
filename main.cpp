acbddb41eda03c45224e9c43a6c1a399fe27245c
Rodrigo Bedoya <rodrigo.bedoya@utec.edu.pe>
2019-03-28 12:19:29 -0500

    finished letter counter

--- main.cpp
+++ main.cpp
@@ -15,18 +15,18 @@ int main(){
     
     	for(int i = 0; i< input.size();i++){
 				if ( letters.find(input[i]) == letters.end() ) {
- 					letters[input[i]] = 0;
- 					chars.push_back(letters[input[i]]);
- 					cout << letters[input[i]];
+ 					letters[input[i]] = 1;
+ 					chars.push_back(input[i]);
  					continue;
 				}			
     			letters[input[i]]++;
 			}
-
+		
 		for(int i = 0; i < chars.size();i++)
 		{
-			cout << chars[i]<<endl;
+			cout << chars[i]<<" " << letters[chars[i]] << endl;
 		}
+
 
 	}
 
